5a042bd2d191b9a662a003909354cc55e9cdc968
kTz1 <catalin_simon90@yahoo.com>
2022-01-28 13:49:39 +0200

    added places search

--- src/App.js
+++ src/App.js
@@ -33,20 +33,22 @@ const App = () => {
     }, [rating]);
 
     useEffect(() => {
-        setIsLoading(true);
-
-        getPlacesData(type, bounds.sw, bounds.ne)
-            .then((data) => {
-                setPlaces(data);
-                setFilteredPlaces([]);
-                setIsLoading(false);
-            })
-    }, [type, coords, bounds]);
+        if(bounds.sw && bounds.ne) {
+            setIsLoading(true);
+    
+            getPlacesData(type, bounds.sw, bounds.ne)
+                .then((data) => {
+                    setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                    setFilteredPlaces([]);
+                    setIsLoading(false);
+                })
+        }
+    }, [type, bounds]);
 
     return (
         <>
             <CssBaseline />
-            <Header />
+            <Header setCoords={setCoords} />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List 
